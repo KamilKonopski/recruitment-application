8166efd792bdd68eb3f70499712fd9caff5ce574
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2025-05-07 00:12:52 +0200

    add layout wrapper

---
+++ Frontend/src/components/Layout/Layout.styled.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const LayoutWrapper = styled.div`
+  max-width: 1920px;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  justify-content: space-between;
+  padding: 20px;
+`;
