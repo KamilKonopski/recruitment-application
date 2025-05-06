d5dd55f48d70166289e7b146e7d75af7d6d8921d
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2025-05-07 00:12:43 +0200

    add main heading

---
+++ Frontend/src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import { MainHeading } from "./Header.styled";
+
+const Header = () => {
+  return (
+    <header>
+      <MainHeading>Aplikacja Rekrutacyjna</MainHeading>
+    </header>
+  );
+};
+
+export default Header;
