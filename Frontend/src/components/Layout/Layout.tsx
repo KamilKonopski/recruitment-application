4fb4f2b757969d71a0d63af1b9d9c31d9e6efcc2
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2025-05-07 00:13:01 +0200

    add layout wrapper

---
+++ Frontend/src/components/Layout/Layout.tsx
@@ -0,0 +1,13 @@
+import Header from "../Header/Header";
+
+import { LayoutWrapper } from "./Layout.styled";
+
+const Layout = () => {
+  return (
+    <LayoutWrapper>
+      <Header />
+    </LayoutWrapper>
+  );
+};
+
+export default Layout;
