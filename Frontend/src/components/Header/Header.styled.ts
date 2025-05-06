ebe9db66d2a1c7f44aa0a0dbf6a87122ffea85ee
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2025-05-07 00:12:31 +0200

    add main heading

---
+++ Frontend/src/components/Header/Header.styled.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const MainHeading = styled.h1`
+  text-align: center;
+  font-size: 35px;
+  font-weight: bold;
+  text-transform: uppercase;
+  margin-top: 50px;
+`;
