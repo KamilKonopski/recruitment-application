fd3f2d2620a1f6369630c1bec243396e6f5bb683
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2025-05-09 01:21:22 +0200

    add button colors

--- Frontend/src/common/styles/theme.ts
+++ Frontend/src/common/styles/theme.ts
@@ -14,6 +14,14 @@ export const theme: DefaultTheme = {
     text: "#111827",
     textMuted: "#6B7280",
 
+    buttonLight: "#111827",
+    buttonBgLight: "#F3F4F6",
+    buttonDark: "#F9FAFB",
+    buttonBgDark: "#111827",
+
+    hoverLight: "#E5E7EB",
+    hoverDark: "#1F2937",
+
     success: "#10B981",
     danger: "#EF4444",
   },
