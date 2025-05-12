70196f258a574aef577fac6e8d48772648864c9b
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2025-05-13 00:02:06 +0200

    add send email function

--- Frontend/src/common/providers/CandidatesProvider.tsx
+++ Frontend/src/common/providers/CandidatesProvider.tsx
@@ -3,6 +3,7 @@ import CandidatesContext from "../context/CandidatesContext";
 import type { ICandidatesBoard, ICandidate } from "../types/candidate";
 
 const LOCAL_STORAGE_KEY = "candidates_data";
+const CANDIDATE_EMAIL_KEY = import.meta.env.VITE_TO_EMAIL;
 
 const defaultCandidates: ICandidatesBoard = {
   application: {
@@ -100,9 +101,44 @@ export const CandidatesProvider = ({
     }));
   };
 
+  const sendEmail = async (subject: string, message: string) => {
+    try {
+      const response = await fetch("http://localhost:3000/api/send", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          to: CANDIDATE_EMAIL_KEY,
+          subject,
+          message,
+        }),
+      });
+
+      const data = await response.json();
+
+      if (!response.ok) {
+        throw new Error(data.error || "Coś poszło nie tak");
+      }
+
+      console.log("Email wysłany pomyślnie: ", data);
+    } catch (error) {
+      console.error(
+        "Błąd podczas wysłania e-maila: ",
+        (error as Error).message
+      );
+    }
+  };
+
   return (
     <CandidatesContext.Provider
-      value={{ candidates, setCandidates, moveToColumn, addNewCandidate }}
+      value={{
+        candidates,
+        setCandidates,
+        moveToColumn,
+        addNewCandidate,
+        sendEmail,
+      }}
     >
       {children}
     </CandidatesContext.Provider>
