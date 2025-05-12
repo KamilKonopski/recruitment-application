f2ee0a104935dad8fa4d5ee1f09542b3622f946b
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2025-05-12 16:58:03 +0200

    add new interfaces

--- Frontend/src/common/types/candidate.ts
+++ Frontend/src/common/types/candidate.ts
@@ -1,12 +1,23 @@
-export type ActionsType = "change" | "send";
-
-export type StatusType = "delivered" | "rejected" | "invited";
-
 export interface ICandidate {
   id: string;
   fullName: string;
-  position: string;
-  status: StatusType;
   email: string;
-  actions: ActionsType;
+  position: string;
+}
+
+export interface ICandidatesColumn {
+  title: string;
+  candidates: ICandidate[];
+}
+
+export interface ICandidatesBoard {
+  application: ICandidatesColumn;
+  interview: ICandidatesColumn;
+  rejected: ICandidatesColumn;
+}
+
+export interface CandidatesContextType {
+  candidates: ICandidatesBoard;
+  setCandidates: React.Dispatch<React.SetStateAction<ICandidatesBoard>>;
+  moveToColumn: (target: keyof ICandidatesBoard, candidate: ICandidate) => void;
 }
