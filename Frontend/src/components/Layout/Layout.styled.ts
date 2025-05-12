c3cf6bd80734fb9b88daf14ef7bf34971ab25df7
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2025-05-12 17:02:13 +0200

    change main and layout styles

--- Frontend/src/components/Layout/Layout.styled.ts
+++ Frontend/src/components/Layout/Layout.styled.ts
@@ -3,10 +3,18 @@ import styled from "styled-components";
 export const LayoutWrapper = styled.div`
   max-width: 1920px;
   width: 100%;
-  height: 100vh;
+  margin: 0 auto;
+  min-height: 100vh;
   display: flex;
   flex-direction: column;
   gap: 20px;
   justify-content: space-between;
-  padding: 20px;
+`;
+
+export const MainWrapper = styled.main`
+  flex-grow: 1;
+  display: flex;
+  justify-content: center;
+  margin: 0 auto;
+  width: 100%;
 `;
